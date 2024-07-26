0d0e32360b4febfa20d58c2fb868e99603a27952
n.alekseev <n.alekseev@aamsystems.ru>
2024-07-26 06:36:34 +0300

    удаляет мусор после внедрнеия CBV

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,5 +1,3 @@
-from django.shortcuts import get_object_or_404, render, redirect
-from django.core.paginator import Paginator
 from django.views.generic import (
     ListView,
     CreateView,
@@ -13,25 +11,21 @@ from .models import Birthday
 from .utils import calculate_birthday_countdown
 
 
-class BirthdayMixin:
+class BirthdayCreateView(CreateView):
     model = Birthday
-    success_url = reverse_lazy("birthday:list")
-
-
-class BirthdayFormMixin:
     form_class = BirthdayForm
-    template_name = "birthday/birthday.html"
-
-
-class BirthdayCreateView(BirthdayMixin, BirthdayFormMixin, CreateView):
     pass
 
 
-class BirthdayUpdateView(BirthdayMixin, BirthdayFormMixin, UpdateView):
+class BirthdayUpdateView(UpdateView):
+    model = Birthday
+    form_class = BirthdayForm
     pass
 
 
-class BirthdayDeleteView(BirthdayMixin, DeleteView):
+class BirthdayDeleteView(DeleteView):
+    model = Birthday    
+    success_url = reverse_lazy('birthday:list')
     pass
 
 
@@ -56,57 +50,4 @@ class BirthdayListView(ListView):
     # ...сортировку, которая будет применена при выводе списка объектов:
     ordering = "id"
     # ...и даже настройки пагинации:
-    paginate_by = 2
-
-
-def birthday_list(request):
-    # Получаем все объекты модели Birthday из БД.
-    birthdays = Birthday.objects.order_by("id")
-    paginator = Paginator(birthdays, 10)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
-    # Передаём их в контекст шаблона.
-    context = {"page_obj": page_obj}
-    return render(request, "birthday/birthday_list.html", context)
-
-
-def birthday(request, pk=None):
-    # Если в запросе указан pk (если получен запрос на редактирование объекта):
-    if pk is not None:
-        # Получаем объект модели или выбрасываем 404 ошибку.
-        instance = get_object_or_404(Birthday, pk=pk)
-    # Если в запросе не указан pk
-    # (если получен запрос к странице создания записи):
-    else:
-        # Связывать форму с объектом не нужно, установим значение None.
-        instance = None
-    # Передаём в форму либо данные из запроса, либо None.
-    # В случае редактирования прикрепляем объект модели.
-    form = BirthdayForm(
-        request.POST or None, files=request.FILES or None, instance=instance
-    )
-    # Остальной код без изменений.
-    context = {"form": form}
-    # Сохраняем данные, полученные из формы, и отправляем ответ:
-    if form.is_valid():
-        form.save()
-        birthday_countdown = calculate_birthday_countdown(form.cleaned_data["birthday"])
-        context.update({"birthday_countdown": birthday_countdown})
-    return render(request, "birthday/birthday.html", context)
-
-
-def delete_birthday(request, pk):
-    # Получаем объект модели или выбрасываем 404 ошибку.
-    instance = get_object_or_404(Birthday, pk=pk)
-    # В форму передаём только объект модели;
-    # передавать в форму параметры запроса не нужно.
-    form = BirthdayForm(instance=instance)
-    context = {"form": form}
-    # Если был получен POST-запрос...
-    if request.method == "POST":
-        # ...удаляем объект:
-        instance.delete()
-        # ...и переадресовываем пользователя на страницу со списком записей.
-        return redirect("birthday:list")
-    # Если был получен GET-запрос — отображаем форму.
-    return render(request, "birthday/birthday.html", context)
+    paginate_by = 10
